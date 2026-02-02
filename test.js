155e6804959b81beb8b1eecbf7c2985152101a76
SA-K1B <tareqshakib789@gmail.com>
2026-02-02 11:36:16 +0600

    add sayMyAge function

--- test.js
+++ test.js
@@ -2,5 +2,9 @@ function sayMyName(name) {
     const name = "Miller";
     console.log("My name is " + name);
 }
+ function sayMyAge(age) {
+    console.log("My age is " + age);
+ }
 
 sayMyName("David");
+sayMyAge(25);
