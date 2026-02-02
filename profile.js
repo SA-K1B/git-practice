ba700d6b98de641bcbdd45f763aaed1589103f98
SA-K1B <tareqshakib789@gmail.com>
2026-02-02 12:38:27 +0600

    Revert "Feat/profile (#5)"
    This reverts commit 67a6337ae5c81d4c35cca119d6545eaac0ca7830.

--- profile.js
+++
@@ -1,7 +0,0 @@
-function profile(name, age, city) {
-    const name = "Miller";
-    const age = "35";
-    const city = "Boston";
-    console.log("My name is " + name + " and my age is " + age + " and I live in " + city);
-}
-profile("David", 25, "New York");
