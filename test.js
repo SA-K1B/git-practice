9a59ff5f390ecf762725af060073da503e490c10
SA-K1B <tareqshakib789@gmail.com>
2026-02-02 12:26:41 +0600

    update settings

--- test.js
+++ test.js
@@ -14,6 +14,7 @@ function loginUser(username, password) {
 loginUser("David", "123456");
 
 function settings() {
+    const user = "Ricky"
     console.log("Welcome to the settings, " + user);
 }
 settings();
