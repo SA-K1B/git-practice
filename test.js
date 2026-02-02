c035ccf8cbf3819624f4a68399e46459b32c1e36
Tarequl Hasan Sakib <tareqshakib789@gmail.com>
2026-02-02 12:04:36 +0600

    Feature/login (#1)
    * add login function
    
    * update username
    
    * update passwd

--- test.js
+++ test.js
@@ -6,6 +6,14 @@ function sayMyName(name) {
     const age = "35";
     console.log("My age is " + age);
  }
+function loginUser(username, password) {
+    const username = "Ricky";
+    const password = "999";
+    console.log("User " + username + " logged in");
+}
+loginUser("David", "123456");
+
+
 
 sayMyName("David");
 sayMyAge(25);
